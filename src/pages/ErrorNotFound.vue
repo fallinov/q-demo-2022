5e811b18e5de8a65e1a35eb9592f1397c5c0a707
SFA <steve.fallet@divtec.ch>
2022-04-27 11:47:15 +0200

    creations pages ex1 et ex2

---
+++ src/pages/ErrorNotFound.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+    <div>
+      <div style="font-size: 30vh">
+        404
+      </div>
+
+      <div class="text-h2" style="opacity:.4">
+        Ooups. Vous êtes perdu ?
+      </div>
+
+      <q-btn
+        class="q-mt-xl"
+        color="red"
+        text-color="white"
+        unelevated
+        to="/"
+        label="Maiiiiisonn"
+        no-caps
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ErrorNotFound'
+})
+</script>
---
+++ src/router/routes.js
@@ -0,0 +1,21 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: 'ex1', component: () => import('pages/Exercice1Page.vue') },
+      { path: 'ex2', component: () => import('pages/Exercice2Page.vue') }
+    ]
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue')
+  }
+]
+
+export default routes
